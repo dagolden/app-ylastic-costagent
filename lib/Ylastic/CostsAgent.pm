48e28d2a36aaa62ff16270696ebd8d4e79d2cc2f
David Golden <dagolden@cpan.org>
2011-03-17 23:26:17 -0400

    initial import of Ylastic-CostsAgent

---
+++ lib/Ylastic/CostsAgent.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+
+package Ylastic::CostsAgent;
+# ABSTRACT: No abstract given for Ylastic::CostsAgent
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+__END__
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use Ylastic::CostsAgent;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
